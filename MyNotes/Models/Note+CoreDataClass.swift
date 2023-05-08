35eec3ebfb2323c601c4bd7c756b94a3f6426b03
AvishkaRavishan <avishk678@icloud.com>
2023-05-09 00:39:20 +0530

    dev files

---
+++ MyNotes/Models/Note+CoreDataClass.swift
@@ -0,0 +1,23 @@
+//
+//  Note+CoreDataClass.swift
+//  StudentNotes
+//
+//  Created by Ravishan S.A.A.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Note)
+public class Note: NSManagedObject {
+    var title: String {
+        return text.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: .newlines).first ?? "" // returns the first line of the text
+    }
+    
+    var desc: String {
+        var lines = text.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: .newlines)
+        lines.removeFirst()
+        return "\(lastUpdated.format()) \(lines.first ?? "")" // return the second ln
+    }
+}
